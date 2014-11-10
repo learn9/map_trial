4b85b6057c28269ca592f4c38b6b5b915b1fd058
kaye wu <apiswswsw@gmail.com>
2014-11-09 21:36:10 -0800

    learning google map

---
+++ map_trial/src/maptrial/Map_trialServlet.java
@@ -0,0 +1,11 @@
+package maptrial;
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class Map_trialServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
